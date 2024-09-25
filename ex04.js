a569e130ab26d7672efd133f36845a192e9fcad1
Tomoya <flyingfox193807@gmail.com>
2024-09-25 12:10:06 -0700

    finished ex04.js

--- ex04.js
+++ ex04.js
@@ -1,3 +1,5 @@
 // Includes Exercise
 // Instructions: Create an array of at least five different fruits as strings. Write code to check if the array includes the fruit "apple".
 
+let array = ["apple", "pineapple", "lime", "grapefruit", "strawberry", "blueberry"]
+console.log(array.includes("apple"))
