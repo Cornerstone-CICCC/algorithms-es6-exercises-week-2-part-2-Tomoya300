21ff98f02dd2ec9f5d1cfd48fddc93d60c670e8f
Tomoya <flyingfox193807@gmail.com>
2024-09-25 11:43:06 -0700

    finished ex01.js

--- ex01.js
+++ ex01.js
@@ -1,2 +1,13 @@
 // Sort Exercise
 // Instructions: Create an array of at least five numbers in random order. Write the code to sort this array in ascending order.
+
+let array = [1, 30, 4, 25, 10000]
+
+function compareFunc(a, b) {
+    if (a > b) return 1
+    if (a < b) return -1
+    if ( a === b) return 0
+}
+
+array.sort(compareFunc)
+console.log(array)
