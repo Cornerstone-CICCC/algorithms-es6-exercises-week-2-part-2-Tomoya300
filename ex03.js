fa704e4207b5db70546b76d105fe8b4589daf163
Tomoya <flyingfox193807@gmail.com>
2024-09-25 12:06:39 -0700

    finished ex03.js

--- ex03.js
+++ ex03.js
@@ -1,3 +1,9 @@
 // Reverse Exercise
 // Instructions: Create an array of at least five numbers. Write code to reverse the order of the elements in the array.
 
+let array = [4, 23, 13, 6, 1, 52, 101, 34]
+let result = array.reverse()
+console.log(result)
+// let sample = ["A", "secret", "makes", "woman", "woman"]
+// let answer = sample.reverse()
+// console.log(answer)
