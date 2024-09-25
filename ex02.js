9d48d8991d9e76b636bffada6ed18e2415bc9fb3
Tomoya <flyingfox193807@gmail.com>
2024-09-25 11:58:30 -0700

    finished ex02.js

--- ex02.js
+++ ex02.js
@@ -1,3 +1,24 @@
 // Find Exercise
 // Instructions: Create an array of at least five numbers. Write code to find the first element that is greater than 10.
 
+let array = [3, 50, 23, 56, 130, 2, 19]
+
+let search = array.find((element) => {
+    return element > 10
+})
+console.log(search)
+
+
+
+
+// array.forEach((item) => {
+//     if (item > 10) {
+//         console.log(item) 
+//     }
+// })
+
+
+// let arr2 = [1, 2, 3, 4, 5, 6]
+// arr2.forEach((item) => {
+//     return item
+// })
