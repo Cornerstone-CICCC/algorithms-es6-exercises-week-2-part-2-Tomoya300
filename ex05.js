6a50d19428b5449d1fbd86c0967c2580f2e4938a
Tomoya <flyingfox193807@gmail.com>
2024-09-25 12:19:01 -0700

    finished ex05.js

--- ex05.js
+++ ex05.js
@@ -1,3 +1,16 @@
 // Some Exercise
 // Instructions: Create an array of at least five numbers. Write code to check if some of the numbers are even.
 
+// let array = [43, 22, 4, 1, 12, 6, 86, 3, 190]
+// let even = ((element) => {
+//     if (element%2 === 0)
+//         return element
+// })
+// console.log(array.some(even))
+
+let array = [43, 22, 4, 1, 12, 6, 86, 3, 190]
+let even = element => {
+    return element%2 === 0
+}
+
+console.log(array.some(even))
