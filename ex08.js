9db8d7a8f6f5ee55c3e7e664fa96e5bd063e0d06
Tomoya <flyingfox193807@gmail.com>
2024-09-25 19:58:46 -0700

    finished ex08.js

--- ex08.js
+++ ex08.js
@@ -1,3 +1,5 @@
 // Join Exercise
 // Instructions: Create an array of at least five different words as strings. Write code to join all the words into a single string, with each word separated by a space.
 
+let array = ["The", "rain", "in", "Spain", "stays", "mainly", "in", "the", "plain"]
+console.log(array.join(" "))
