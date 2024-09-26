f6fea36b5d55dbba749af367fc8451fc33514b87
Tomoya <flyingfox193807@gmail.com>
2024-09-25 19:55:06 -0700

    finished ex07.js

--- ex07.js
+++ ex07.js
@@ -1,3 +1,5 @@
 // IndexOf Exercise
 // Instructions: Create an array of at least five different fruits as strings. Write code to find the index of the fruit "banana".
 
+let array = ["apple", "pineapple", "lime", "grapefruit", "strawberry", "banana", "blueberry"]
+console.log(array.indexOf("banana"))
